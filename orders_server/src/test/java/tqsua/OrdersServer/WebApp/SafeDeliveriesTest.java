87fd666a0116a68a1fb66589f71a435e07333af6
Ricardo <ricardo.cruz29@ua.pt>
2021-06-05 23:51:26 +0100

    Cucumber+Selenium webTests done. Very similar to deliveries WebApp, but with the necessary changes

---
+++ orders_server/src/test/java/tqsua/OrdersServer/WebApp/SafeDeliveriesTest.java
@@ -0,0 +1,12 @@
+package tqsua.OrdersServer.WebApp;
+
+//This class is commented so that the web tests wont run. This must be changed as soon as possible
+
+import io.cucumber.junit.platform.engine.Cucumber;
+
+@Cucumber
+public class SafeDeliveriesTest {
+
+
+}
+
