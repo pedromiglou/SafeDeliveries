5f97676ba356c53f19b93cd95c9bf154476978d3
Rafael Baptista <rafaelbaptista@ua.pt>
2021-06-06 23:43:47 +0100

    Connection Web Auth to Api

--- deliveries_webapp/src/Components/Login/Login.js
+++ deliveries_webapp/src/Components/Login/Login.js
@@ -1,44 +1,102 @@
 /* css */
 import { useState } from 'react';
 import './Login.css';
+import AuthService from "../../Services/auth.service";
 
 function Login() {
     const [state, setState] = useState("Login");
+    const [sucessRegister, setSucessRegister] = useState(false);
+    const [errorRegister, setErrorRegister] = useState(false);
+
+    async function login() {
+      var response = await AuthService.login(
+        document.getElementById("login_email_input").value,
+        document.getElementById("login_password_input").value
+      )   
+
+      console.log(response);
+    }
+
+    async function registerFunction(event) {
+      event.preventDefault();
+      await register();
+    }
+
+    async function register() {
+      var response = await AuthService.register(
+        document.getElementById("register_firstname_input").value,
+        document.getElementById("register_lastname_input").value,
+        document.getElementById("register_email_input").value,
+        document.getElementById("register_password_input").value
+      )   
+      if (!response.error) {
+        setSucessRegister(true);
+        setState("Login")
+      } else {
+        setSucessRegister(false);
+        setErrorRegister(true);
+      }
+    }
+
+    function validatePassword(){
+      var password = document.getElementById("register_password_input")
+        , confirm_password = document.getElementById("register_confirmpass_input");
+      if(password.value !== confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+      } else {
+        confirm_password.setCustomValidity('');
+      }
+    }
+
+    
 
     return (
       <>
+
+        {sucessRegister === true 
+          ? <div className="alert alert-success" role="alert" style={{margin:"10px auto", width: "90%", textAlign:"center", fontSize:"22px"}}>
+          A sua conta foi criada com sucesso! 
+          </div> : null}
+
+        {errorRegister === true 
+          ? <div className="alert alert-alert" role="alert" style={{margin:"10px auto", width: "90%", textAlign:"center", fontSize:"22px"}}>
+          Ocorreu um erro ao criar a sua conta.
+          </div> : null}
+
         <div className="LoginSection">
             { state === "Login" && 
-            <div className="Login_Register">
-                <h2>Login</h2>
-                <hr></hr>
-                <h3>Name</h3>
-                <input type="text" placeholder="Name"></input>
-                <h3>Password</h3>
-                <input type="password" placeholder="Password"></input>
-                <i onClick={() => setState("Register")}>Don't have an account? Regist here</i>
-                <button className="button-entrar">Enter</button>
-            </div>
+            <form onSubmit={login}>
+              <div className="Login_Register">
+                  <h2>Login</h2>
+                  <hr></hr>
+                  <h3>Email</h3>
+                  <input type="text" id="login_email_input" placeholder="Email"></input>
+                  <h3>Password</h3>
+                  <input type="password" id="login_password_input" placeholder="Password"></input>
+                  <i onClick={() => setState("Register") }>Don't have an account? Regist here</i>
+                  <button type="submit" className="button-entrar">Enter</button>
+              </div>
+            </form>
             }
             { state === "Register" && 
-            <div className="Login_Register">
-                <h2>Register</h2>
-                <hr></hr>
-                <h3>Name</h3>
-                <input type="text" placeholder="Name"></input>
-                <h3>Email</h3>
-                <input type="text" placeholder="Email"></input>
-                <h3>Password</h3>
-                <input type="password" placeholder="Password"></input>
-                <h3>Confirm Password</h3>
-                <input type="password" placeholder="Confirm Password"></input>
-                <h3>Veiculo</h3>
-                <select>
-                  <option selected defaultValue>Choose vehicle</option>
-                </select>
-                <i onClick={() => setState("Login")}>Already have an account? Click here</i>
-                <button className="button-entrar">Create</button>
-            </div>
+            <form onSubmit={registerFunction}>
+              <div className="Login_Register">
+                  <h2>Register</h2>
+                  <hr></hr>
+                  <h3>First Name</h3>
+                  <input type="text" id="register_firstname_input" placeholder="First name" required></input>
+                  <h3>Last Name</h3>
+                  <input type="text" id="register_lastname_input" placeholder="Last name" required></input>
+                  <h3>Email</h3>
+                  <input type="text" id="register_email_input" placeholder="Email" required></input>
+                  <h3>Password</h3>
+                  <input onChange={validatePassword} type="password" id="register_password_input" placeholder="Password" required></input>
+                  <h3>Confirm Password</h3>
+                  <input onKeyUp={validatePassword} type="password" id="register_confirmpass_input" placeholder="Confirm Password" required></input>
+                  <i onClick={() => setState("Login")}>Already have an account? Click here</i>
+                  <button type="submit" className="button-entrar">Create</button>
+              </div>
+            </form>
             }
             
         </div>
---
+++ deliveries_webapp/src/Services/auth.service.js
@@ -0,0 +1,53 @@
+
+class AuthService {
+
+    async login(email, password) {
+        let userInfo= {
+            email: email,
+            password: password,
+        }
+
+        var res = await fetch('http://localhost:8080/api/login', {
+            method:'POST',
+            headers:{'Content-type':'application/json'},
+            body: JSON.stringify(userInfo)
+        })
+
+        var json = await res.json()
+        
+        if(json.token) {
+            sessionStorage.setItem("user", JSON.stringify(json));
+        } 
+        return json
+
+    }
+
+    async register(firstname, lastname, email, password) {
+        let userInfo= {
+            firstname: firstname,
+            lastname: lastname,
+            email: email,
+            password: password,
+        }
+        try {
+            var res = await fetch('http://localhost:8080/api/registo', {
+                method:'POST',
+                headers:{'Content-type':'application/json'},
+                body: JSON.stringify(userInfo)
+            })
+        } catch {
+            return {error: true};
+        }
+
+        if (res.status !== 200) {
+            return {error: true};
+        }
+        var json = await res.json()
+    
+        return json
+
+    }
+
+}
+
+export default new AuthService()
