29f8e72d9beb7168d0e10aa74856b47e29733f25
Rafael Baptista <rafaelbaptista@ua.pt>
2021-06-07 16:59:02 +0100

    Correction in VehiclesControllerTest due to pipeline failure

--- deliveries_server/src/test/java/tqsua/DeliveriesServer/controller/VehicleControllerTest.java
+++ deliveries_server/src/test/java/tqsua/DeliveriesServer/controller/VehicleControllerTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -26,7 +28,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(VehicleController.class)
+//@WebMvcTest(VehicleController.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class VehicleControllerTest {
 
     @Autowired
