b42cced03145c864c93f97842a646e27c0cc6b97
Rafael Baptista <rafaelbaptista@ua.pt>
2021-06-22 17:18:26 +0100

    Updated Home pages introduction texts

--- orders_webapp/src/Components/Home/Home.js
+++ orders_webapp/src/Components/Home/Home.js
@@ -7,7 +7,8 @@ function Home() {
         <div className="HomeSection">
             <div>
                 <h1>SafeDeliveries</h1>
-                <i>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</i>
+                <i>Welcome to SafeDeliveries Client side webapp! Here you are able to create your own orders, adding the necessary items to them and selecting a pick up and deliver address of your own choice. <br />
+                  You just need to have one thing in your mind. Since we are SafeDeliveries, Receive your orders safely!</i>
             </div>
             <div>
                 <img className="background-img" src={process.env.PUBLIC_URL + "/images/background/Delivery.jpg"} alt="background-img"></img>
